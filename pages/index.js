8c0ca91124549c0e8ed5073aacbd53c3b71f9b5c
0limpi0 <olymangue@gmail.com>
2021-02-22 15:20:58 +0200

    add Home page contents and styles

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,72 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+import Image from 'next/image'
 
-export default function Home() {
+export async function getStaticProps() {
+  const url = 'https://restcountries.eu/rest/v2/all';
+
+  const response = await fetch(url);
+  const data = await response.json();
+  const countries = data;
+  return {
+    props: {
+      countries
+    }
+  }
+}
+
+export default function Home({ countries }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Explore countries</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <Header />
+      <main className={styles.homeContainer}>
+        <section className={styles.inputContainer}>
+          <input
+            className={styles.inputSearch}
+            type="text"
+            placeholder="Search for a country..."
+          />
+          <input
+            className={styles.inputFilter}
+            type="text"
+            placeholder="Filter by Region"
+          />
+        </section>
+        <section className={styles.countriesContainer}>
+          {countries.slice(0, 8).map(country => (
+            <aside className={styles.countryItem} key={country.numericCode}>
+              <div className={styles.countryFlag}>
+                <Image
+                  src={country.flag}
+                  width={237}
+                  height={144}
+                />
+              </div>
+              <div className={styles.countryInfo}>
+                <h4 className={styles.countryName}>
+                  {country.name}
+                </h4>
+                <p className={styles.countryPopulation}>
+                  <span>Population: </span>{country.population}
+                </p>
+                <p className={styles.countryRegion}>
+                  <span>Region: </span>{country.region}
+                </p>
+                <p className={styles.countryCapital}>
+                  <span>Capital: </span>{country.capital}
+                </p>
+              </div>
+            </aside>
+          ))}
+        </section>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    </div >
   )
 }
